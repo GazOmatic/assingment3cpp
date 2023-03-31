1bac1e7aa36e97e827ac7669b8138bcf4c374878
Gareth <garethw738@gmail.com>
2023-03-31 17:49:21 +0200

    Added a filter test

--- driver.cpp
+++ driver.cpp
@@ -3,7 +3,22 @@
 using namespace WRBGAR002;
 int main(int argc, char const *argv[])
 {
-    PGMimageProcessor h = PGMimageProcessor("chess.pgm");
-    h.extractComponents(200,100);
+    PGMimageProcessor h = PGMimageProcessor("chess2.pgm");
+    int thresh;
+    int min;
+    if (argc < 2)
+    {
+        thresh = 200;
+        min = 50;
+    }
+    else
+    {
+        thresh = std::stoi(argv[1]);
+        min = std::stoi(argv[2]);
+    }
+    h.extractComponents(thresh, min);
+    h.filterComponentBySize(20, 100);
+
+    print(h.components.size());
     return 0;
 }
