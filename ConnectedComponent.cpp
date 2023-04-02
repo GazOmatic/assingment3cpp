59cb9463e98a3b145804d38b02dd18cba3134863
Gareth <garethw738@gmail.com>
2023-04-02 09:00:56 +0200

    Made a seperate header file for ConnectedComponent

--- ConnectedComponent.cpp
+++ ConnectedComponent.cpp
@@ -1,27 +1,51 @@
-#include <set>
-typedef std::set<std::pair<int,int>> setPair;
-class ConnectedComponent
+#include "ConnectedComponent.h"
+#define CC ConnectedComponent::ConnectedComponent
+
+CC()
 {
-public:
-    int pixelCount;
-    int id;
+    pixelCount = 0;
+    id = 0;
     setPair pixels;
+}
+CC(const ConnectedComponent &rhs) : pixelCount(rhs.pixelCount), id(rhs.id), pixels(rhs.pixels) {}
 
-    ConnectedComponent() : pixelCount(0), id(0) {}
+ConnectedComponent &ConnectedComponent::operator=(const ConnectedComponent &rhs)
+{
+    if (this != &rhs)
+    {
+        this->pixelCount = rhs.pixelCount;
+        this->id = rhs.id;
+        this->pixels = rhs.pixels;
+    }
+    return *this;
+}
+CC(ConnectedComponent &&rhs) : pixelCount(rhs.pixelCount), id(rhs.id), pixels(std::move(rhs.pixels)) {}
 
-    ConnectedComponent &operator+=(const ConnectedComponent &rhs)
+ConnectedComponent &ConnectedComponent::operator=(ConnectedComponent &&rhs)
+{
+    if (this != &rhs)
     {
-        this->pixelCount += rhs.pixelCount;
-        this->pixels.insert(rhs.pixels.begin(), rhs.pixels.end());
-        return *this;
+        this->pixelCount = rhs.pixelCount;
+        this->id = rhs.id;
+        this->pixels = std::move(rhs.pixels);
     }
-    ConnectedComponent operator+(const ConnectedComponent &rhs) const
-    {
-        ConnectedComponent result = *this;
-        result += rhs;
-        return result;
-    }
-};
+    return *this;
+}
+ConnectedComponent::~ConnectedComponent() {}
+ConnectedComponent &ConnectedComponent::operator+=(const ConnectedComponent &rhs)
+{
+    this->pixelCount += rhs.pixelCount;
+    this->pixels.insert(rhs.pixels.begin(), rhs.pixels.end());
+    return *this;
+}
+ConnectedComponent ConnectedComponent::operator+(const ConnectedComponent &rhs) const
+{
+    ConnectedComponent result = *this;
+    result += rhs;
+    return result;
+}
+
+// Easy way to add pairs together
 setPair operator+(setPair &lhs, setPair &rhs)
 {
     setPair out;
---
+++ ConnectedComponent.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <set>
+
+typedef std::set<std::pair<int, int>> setPair;
+
+class ConnectedComponent
+{
+public:
+    int pixelCount;
+    int id;
+    setPair pixels;
+    ConnectedComponent();
+    ConnectedComponent(const ConnectedComponent &rhs);
+    ConnectedComponent &operator=(const ConnectedComponent &rhs);
+    ConnectedComponent(ConnectedComponent &&rhs);
+    ConnectedComponent &operator=(ConnectedComponent &&rhs);
+    ~ConnectedComponent(){};
+    ConnectedComponent &operator+=(const ConnectedComponent &rhs);
+    ConnectedComponent operator+(const ConnectedComponent &rhs) const;
+};
